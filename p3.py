3d9a5c5aed0f90b3dae4d6917470b8783c1f4c4f
shubham gangwar <shubhamgangwar989795@gmail.com>
2026-04-03 17:44:14 +0530

    updated

--- p3.py
+++ p3.py
@@ -6,3 +6,21 @@ print(a+b)
 a = 9
 b = 3
 print(a-b)
+
+city = "bareilly"
+temp = 45
+#message = the temperature in bareilly is 45 degree
+message = "the temperature in " + str(city) + " is " + str(temp) + " degree"
+print(message)
+message = f"the temperature in {city} is {temp} degree"
+print(message)
+ 
+#name = don't worry about it.
+name = 'don\'t worry about it'
+print(name)
+name = 'don\n''t worry about it'
+print(name)
+name = 'don\'t\nworry about it'
+print(name)
+name = 'dont w\norry about it'
+print(name)
